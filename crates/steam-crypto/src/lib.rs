d5b99909d77fead1f8cd16dda1618eab2c1f08da
Martin <martin@hotmail.com.br>
2021-01-27 18:31:44 -0300

    refactor (steam-crypto): better error handling, new functions
    Signed-off-by: Martin <martin@hotmail.com.br>

--- crates/steam-crypto/src/lib.rs
+++ crates/steam-crypto/src/lib.rs
@@ -6,26 +6,30 @@
 //! Direct Port of
 //! https://github.com/DoctorMcKay/node-steam-crypto
 
-#[allow(unused_imports)]
+
+#![warn(missing_docs, missing_doc_code_examples)]
+#![deny(
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications
+)]
+
+extern crate crc32fast;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
 extern crate lazy_static_include;
 
-use std::fs::OpenOptions;
-use std::io::Read;
-
-use openssl::error::ErrorStack;
-use openssl::hash::MessageDigest;
-use openssl::rsa::{Padding, Rsa, RsaRef};
-use openssl::sha::Sha1;
-use openssl::sign::Verifier;
-use openssl::symm::{Cipher, Crypter, Mode};
+use bytes::{BufMut, Bytes, BytesMut};
+use crc32fast::Hasher;
+use openssl::{error::ErrorStack, hash::MessageDigest, rsa::Padding, sign::Verifier};
 use rand::prelude::*;
 
 mod symm;
-use symm::*;
-
 lazy_static_include_bytes!(STEAM_KEY, "assets/steam_public.pem");
 
 #[derive(Debug)]
@@ -35,44 +39,62 @@ pub struct SessionKeys {
 }
 
 pub fn verify_signature(data: &[u8], signature: &[u8]) -> Result<bool, ErrorStack> {
-    /// standard algorithm is RSA-SHA1
-    /// but this should be selectable
+    // standard algorithm is RSA-SHA1
+    // but this should be selectable
     let steam_key_bytes: &'static [u8] = *STEAM_KEY;
-    let steam_pkey = openssl::pkey::PKey::public_key_from_pem(&steam_key_bytes).unwrap();
+    let steam_pkey = openssl::pkey::PKey::public_key_from_pem(&steam_key_bytes)?;
 
-    let mut verifier = Verifier::new(MessageDigest::sha1(), &steam_pkey).unwrap();
-    verifier.update(&data).unwrap();
+    let mut verifier = Verifier::new(MessageDigest::sha1(), &steam_pkey)?;
+    verifier.update(&data)?;
     verifier.verify(&signature)
 }
 
-/// Generates a 32 byte random blob of data and encrypts it with RSA using the Steam system's public key.
+/// Generates a 32 byte random blob of data and encrypts it with RSA 1024
+/// using the Steam system's public key.
+/// If there is a nonce, it gets concatenated after the generated 32 bytes
 /// Returns SessionsKeys struct.
-pub fn generate_session_key(nonce: &str) -> SessionKeys {
-    /// If there is a nonce, it gets concatenated after the generated 32 bytes
-    let mut random_bytes_array: Vec<u8> = vec![0; 32];
-    let mut encrypted_array: Vec<u8> = vec![0; 256];
+pub fn generate_session_key(nonce: Option<&[u8]>) -> Result<SessionKeys, ErrorStack> {
+    let mut random_bytes_array = vec![0u8; 32];
+    let mut encrypted_array = vec![0u8; 256];
 
     thread_rng().fill_bytes(&mut random_bytes_array);
 
-    random_bytes_array.extend(nonce.bytes());
+    if let Some(nonce) = nonce {
+        random_bytes_array.extend(nonce);
+    }
 
     let steam_key: &'static [u8] = *STEAM_KEY;
-    let public_key = openssl::rsa::Rsa::public_key_from_pem(&steam_key).unwrap();
-    RsaRef::public_encrypt(&public_key, &random_bytes_array, &mut encrypted_array, Padding::PKCS1)
-        .unwrap();
+    let public_key = openssl::rsa::Rsa::public_key_from_pem(&steam_key)?;
+    public_key.public_encrypt(&random_bytes_array, &mut encrypted_array, Padding::PKCS1)?;
 
-    SessionKeys { plain_text: random_bytes_array, encrypted: encrypted_array }
+    Ok(SessionKeys {
+        plain_text: random_bytes_array,
+        encrypted: encrypted_array,
+    })
 }
 
-#[cfg(test)]
-mod tests {
-    use rand::rngs::StdRng;
-    use rand::SeedableRng;
+/// Performs CRC32 on an input byte array
+pub fn crc_hash(input: &[u8]) -> [u8; 4] {
+    let mut hasher = Hasher::new();
+    hasher.update(&input);
 
-    use crate::generate_session_key;
+    let checksum = hasher.finalize();
+    let mut checksum_bytes: [u8; 4] = checksum.to_be_bytes();
+    checksum_bytes.reverse();
+    checksum_bytes
+}
 
-    #[test]
-    fn it_works() {
-        println!("{:?}", generate_session_key(&"kkkkkkkkkk").plain_text)
-    }
+/// Returns ready to send payload for MsgEncryptRequest
+pub fn generate_encrypt_request_handshake(payload: &[u8]) -> (SessionKeys, Bytes) {
+    let session_keys = generate_session_key(Some(payload)).unwrap();
+    let temp_encrypted_sessionkey = &session_keys.encrypted[..128];
+
+    let mut response = BytesMut::with_capacity(1024);
+    let key_hash = crc_hash(temp_encrypted_sessionkey);
+
+    response.put(temp_encrypted_sessionkey);
+    // ugly.. fix
+    response.put(key_hash.to_vec().as_slice());
+    response.put_u32(0);
+    (session_keys, response.freeze())
 }
--- crates/steam-crypto/src/symm/mod.rs
+++ crates/steam-crypto/src/symm/mod.rs
@@ -1,67 +1,80 @@
-#![warn(dead_code)]
 
-use openssl::hash::MessageDigest;
-use openssl::sign::Signer;
-use openssl::symm::{Cipher, Crypter, Mode};
+use openssl::{
+    error::ErrorStack,
+    hash::MessageDigest,
+    sign::Signer,
+    symm::{Cipher, Crypter, Mode},
+};
 use rand::{thread_rng, RngCore};
 
+type Result<T> = std::result::Result<T, ErrorStack>;
+
 /// Encrypt or decrypt a message with AES 256 CBC.
-fn cipher_message(message: &[u8], key: &[u8], plain_iv: Option<&[u8]>, mode: Mode) -> Vec<u8> {
-    let mut message_cipher = Crypter::new(Cipher::aes_256_cbc(), mode, key, plain_iv).unwrap();
+pub fn cipher_message(
+    message: &[u8],
+    key: &[u8],
+    plain_iv: Option<&[u8]>,
+    mode: Mode,
+) -> Result<Vec<u8>> {
+    let mut message_cipher = Crypter::new(Cipher::aes_256_cbc(), mode, key, plain_iv)?;
 
     let mut output_buffer: Vec<u8> = Vec::new();
 
-    message_cipher.update(&message, &mut output_buffer);
-    message_cipher.finalize(&mut output_buffer).unwrap();
-    output_buffer
+    message_cipher.update(&message, &mut output_buffer)?;
+    message_cipher.finalize(&mut output_buffer)?;
+    Ok(output_buffer)
 }
 
 /// Encrypt or decrypt an Initialization Vector with AES 256 ECB.
-fn cipher_iv_ecb(key: &[u8], plain_iv: Option<&[u8]>, mode: Mode) -> Vec<u8> {
+fn cipher_iv_ecb(key: &[u8], plain_iv: Option<&[u8]>, mode: Mode) -> Result<Vec<u8>> {
     let mut iv_cipher = Crypter::new(Cipher::aes_256_ecb(), mode, key, plain_iv).unwrap();
     iv_cipher.pad(false);
 
     let mut output_buffer: Vec<u8> = Vec::new();
 
-    iv_cipher.update(plain_iv.unwrap(), &mut output_buffer);
+    iv_cipher.update(plain_iv.unwrap(), &mut output_buffer)?;
     iv_cipher.finalize(&mut output_buffer).unwrap();
-    output_buffer
+    Ok(output_buffer)
 }
 
 pub fn symmetric_encrypt(input: &[u8], key: &[u8]) -> Vec<u8> {
     let mut iv: [u8; 16] = [0; 16];
     thread_rng().fill_bytes(&mut iv);
 
-    symmetric_encrypt_with_iv(input, key, Option::from(&iv[..]))
+    symmetric_encrypt_with_iv(input, key, Option::from(&iv[..])).unwrap()
 }
 
-pub fn symmetric_encrypt_with_iv(message: &[u8], key: &[u8], plain_iv: Option<&[u8]>) -> Vec<u8> {
-    let encrypted_iv = cipher_iv_ecb(key, plain_iv, Mode::Encrypt);
-    let encrypted_message = cipher_message(message, key, plain_iv, Mode::Encrypt);
+pub fn symmetric_encrypt_with_iv(
+    message: &[u8],
+    key: &[u8],
+    plain_iv: Option<&[u8]>,
+) -> Result<Vec<u8>> {
+    let encrypted_iv = cipher_iv_ecb(key, plain_iv, Mode::Encrypt)?;
+    let encrypted_message = cipher_message(message, key, plain_iv, Mode::Encrypt)?;
 
     let mut output = encrypted_iv;
     output.extend(encrypted_message.into_iter());
-    output
+    Ok(output)
 }
 
-pub fn symmetric_decrypt(input: &[u8], key: &[u8], is_hmac: bool) -> Vec<u8> {
+pub fn symmetric_decrypt(input: &[u8], key: &[u8], is_hmac: bool) -> Result<Vec<u8>> {
     let encrypted_iv = &input[0..16];
-    let plain_iv = cipher_iv_ecb(key, Option::from(encrypted_iv), Mode::Decrypt);
+    let plain_iv = cipher_iv_ecb(key, Some(encrypted_iv), Mode::Decrypt)?;
 
     let encrypted_message = &input[16..];
 
     if !is_hmac {
-        cipher_message(encrypted_message, key, Option::from(encrypted_iv), Mode::Decrypt);
+        cipher_message(encrypted_message, key, Some(encrypted_iv), Mode::Decrypt)?;
     }
     let iv_len = plain_iv.len();
     let hmac_partial = &plain_iv[..iv_len - 3];
     let hmac_random_bytes = &plain_iv[iv_len - 3..];
 
-    let signed_data = sign_hmac_sha1(&hmac_random_bytes, &plain_iv, &key[..16]);
+    let signed_data = sign_hmac_sha1(&hmac_random_bytes, &plain_iv, &key[..16]).unwrap();
     if &signed_data[..iv_len] != hmac_partial {
         panic!("Received invalid HMAC from remote host.");
     }
-    plain_iv
+    Ok(plain_iv)
 }
 
 /// Encrypt input with key. Returns HMAC
@@ -71,20 +84,20 @@ pub fn symmetric_encrypt_hmac_iv(input: &[u8], key: &[u8]) -> Vec<u8> {
     let mut random_vec: [u8; RAND_VEC_SIZE] = [0; RAND_VEC_SIZE];
     thread_rng().fill_bytes(&mut random_vec);
 
-    let signed_data = sign_hmac_sha1(&mut random_vec, &input, &key[..16]);
+    let signed_data = sign_hmac_sha1(&mut random_vec, &input, &key[..16]).unwrap();
 
     // the resulting IV must be 16 bytes long, so truncate the hmac to make room for the random
     let mut signed_data_slice = signed_data[..16 - RAND_VEC_SIZE].to_vec();
     signed_data_slice.extend(random_vec.iter());
 
-    symmetric_encrypt_with_iv(input, key, Option::from(signed_data_slice.as_ref()))
+    symmetric_encrypt_with_iv(input, key, Some(signed_data_slice.as_ref())).unwrap()
 }
 
-fn sign_hmac_sha1(random_bytes: &[u8], input: &[u8], key: &[u8]) -> Vec<u8> {
-    let pkey = openssl::pkey::PKey::hmac(&key).unwrap();
-    let mut signer = Signer::new(MessageDigest::sha1(), &pkey).unwrap();
-    signer.update(random_bytes).unwrap();
-    signer.update(&input).unwrap();
-    let signed_data = signer.sign_to_vec().unwrap();
-    signed_data
+fn sign_hmac_sha1(random_bytes: &[u8], input: &[u8], key: &[u8]) -> Result<Vec<u8>> {
+    let pkey = openssl::pkey::PKey::hmac(&key)?;
+    let mut signer = Signer::new(MessageDigest::sha1(), &pkey)?;
+    signer.update(random_bytes)?;
+    signer.update(&input)?;
+    let signed_data = signer.sign_to_vec()?;
+    Ok(signed_data)
 }
