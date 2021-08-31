b955ecd9ab6b0f14e855f00ea03018d171203c16
Martin <martin@hotmail.com.br>
2021-08-31 13:39:37 -0300

    minor changes to generator and generate fns

--- crates/steam-language-gen/src/generate.rs
+++ crates/steam-language-gen/src/generate.rs
@@ -5,13 +5,13 @@ fn main() {
     let path_enums_output: &'static str = "crates/steam-language-gen/src/generated/";
     let path_steamsg_output: &'static str = "crates/steam-language-gen/src/generated/messages.rs";
 
-    let file_steam_msg: &'static str = include_str!("../../assets/SteamKit/Resources/SteamLanguage/steammsg.steamd");
+    let file_steam_msg: &'static str = include_str!("../assets/SteamKit/Resources/SteamLanguage/steammsg.steamd");
 
-    let file_eresult: &'static str = include_str!("../../assets/SteamKit/Resources/SteamLanguage/eresult.steamd");
+    let file_eresult: &'static str = include_str!("../assets/SteamKit/Resources/SteamLanguage/eresult.steamd");
 
-    let file_emsg: &'static str = include_str!("../../assets/SteamKit/Resources/SteamLanguage/emsg.steamd");
+    let file_emsg: &'static str = include_str!("../assets/SteamKit/Resources/SteamLanguage/emsg.steamd");
 
-    let file_steam_enums: &'static str = include_str!("../../assets/SteamKit/Resources/SteamLanguage/enums.steamd");
+    let file_steam_enums: &'static str = include_str!("../assets/SteamKit/Resources/SteamLanguage/enums.steamd");
 
     let (class_graph, class_entry) = parse_classes_to_tree(file_steam_msg);
     let file_class = generate_code(class_graph, class_entry);
