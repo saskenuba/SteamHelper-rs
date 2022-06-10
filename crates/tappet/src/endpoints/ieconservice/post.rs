4bae88942672f2196733df6bc58a82c5a2a7bdf0
Martin <martin@hotmail.com.br>
2022-06-10 02:25:02 -0300

    chore!: removed the now deprecated endpoints CancelTradeOffer and ..
    DeclineTradeOffer;
    
    Signed-off-by: Martin <martin@hotmail.com.br>

--- crates/tappet/src/endpoints/ieconservice/post.rs
+++ crates/tappet/src/endpoints/ieconservice/post.rs
@@ -6,23 +6,3 @@ new_type!(IEconService);
 
 impl_conversions!(@PostQueryBuilder -> @IEconService);
 convert_with_endpoint!(@PostQueryBuilder -> @IEconService);
-
-#[interface(IEconService)]
-#[derive(Parameters, Serialize, Debug, Default)]
-#[doc(hidden)]
-pub struct DeclineTradeOfferParameters {
-    tradeofferid: i64,
-}
-
-#[interface(IEconService)]
-#[derive(Parameters, Serialize, Debug, Default)]
-#[doc(hidden)]
-pub struct CancelTradeOfferParameters {
-    tradeofferid: i64,
-}
-
-convert_with_endpoint!(@IEconService -> CancelTradeOffer |> "CancelTradeOffer/v1");
-convert_with_endpoint!(@IEconService -> DeclineTradeOffer |> "DeclineTradeOffer/v1");
-
-impl_executor!(CancelTradeOffer);
-impl_executor!(DeclineTradeOffer);
