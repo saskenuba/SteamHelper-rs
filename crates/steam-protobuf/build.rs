01e22f1e0f8e5a13f6d67e745ac10e4ea25f29da
Martin <martin@hotmail.com.br>
2024-01-16 16:25:15 -0300

    feat!: regenerated all steam protobufs, new build.rs

---
+++ crates/steam-protobuf/build.rs
@@ -0,0 +1,31 @@
+use std::path::PathBuf;
+
+use protobuf_codegen::Customize;
+
+fn main() {
+    let mut entries: Vec<PathBuf> = Vec::new();
+
+    let file_iterator = glob::glob("**/steam/*.proto").unwrap().filter_map(|path| path.ok());
+    for entry in file_iterator {
+        eprintln!("{:?}", entry);
+        entries.push(entry);
+    }
+    eprintln!("{:?}", entries);
+
+    let protoc_cfg = Customize::default()
+        .generate_accessors(true)
+        .tokio_bytes(true)
+        .inside_protobuf(true);
+
+    // Use this in build.rs
+    protobuf_codegen::Codegen::new()
+        .customize(protoc_cfg)
+        .protoc()
+        .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
+        .capture_stderr()
+        .inputs(entries)
+        .includes(&["assets/protobufs/steam", "assets/protobufs/google"])
+        .out_dir("src/protobufs")
+        .run()
+        .unwrap();
+}
--- crates/steam-protobuf/src/lib.rs
+++ crates/steam-protobuf/src/lib.rs
@@ -1,6 +1,6 @@
-#[macro_use]
-extern crate serde;
-
-pub use protobuf::Message;
-
-pub mod steam;
+// #[macro_use]
+// extern crate serde;
+//
+// pub use protobuf::Message;
+//
+// pub mod steam;
