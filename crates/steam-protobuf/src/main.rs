3bfd5d1b313f1499bdacad03645693e8758f3a23
Martin <martin@hotmail.com.br>
2024-01-23 17:06:43 -0300

    fix: fixed steam-protobuf main.rs better under feature-flag
    Signed-off-by: Martin <martin@hotmail.com.br>

--- crates/steam-protobuf/src/main.rs
+++ crates/steam-protobuf/src/main.rs
@@ -1,33 +1,11 @@
-#![cfg(feature = "regen")]
+cfg_if::cfg_if! {
+ if #[cfg(feature = "regen")] {
 
-use std::path::PathBuf;
+        mod regen;
+        fn main() { regen::generate() }
 
-use protobuf_codegen::Customize;
-
-fn main() {
-    let mut entries: Vec<PathBuf> = Vec::new();
-
-    let file_iterator = glob::glob("**/steam/*.proto").unwrap().filter_map(|path| path.ok());
-    for entry in file_iterator {
-        eprintln!("{:?}", entry);
-        entries.push(entry);
     }
-    eprintln!("{:?}", entries);
-
-    let protoc_cfg = Customize::default()
-        .generate_accessors(true)
-        .inside_protobuf(true)
-        .tokio_bytes(true);
-
-    // Use this in build.rs
-    protobuf_codegen::Codegen::new()
-        .customize(protoc_cfg)
-        .protoc()
-        .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
-        .capture_stderr()
-        .inputs(entries)
-        .includes(&["assets/protobufs/steam", "assets/protobufs/google"])
-        .out_dir("src/protobufs")
-        .run()
-        .unwrap();
+    else {
+        fn main() { println!(r#"Enable feature "regen" in order to regenerate protobufs."#)}
+    }
 }
